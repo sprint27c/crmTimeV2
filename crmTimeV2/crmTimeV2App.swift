766959734576e5788f83673c4fe4392c3f4c6c27
sprint27c <stccrm@gmail.com>
2024-05-13 15:02:29 -0500

    Initial Commit

---
+++ crmTimeV2/crmTimeV2App.swift
@@ -0,0 +1,17 @@
+//
+//  crmTimeV2App.swift
+//  crmTimeV2
+//
+//  Created by Shawn CRM on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct crmTimeV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
